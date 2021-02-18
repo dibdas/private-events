65d74820ab8f2a8eec3e7f279948185b6e0494a5
udberg <johnuduak066@gmail.com>
2021-02-18 12:16:57 +0100

    Add event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,6 @@
 class Event < ApplicationRecord
+    belongs_to :creator,class_name :'User'
+    has_many :attendees, through: :invitation, source: :User
+    scope :past, ->{where('date < ?', Date.today)}
+    scope :upcoming, ->{where('date>=?', Date.today)}
 end
