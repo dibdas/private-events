98e6c3aac0e58af578f838c88953813d3ed68f59
dibdas <dibyandu08das@gmail.com>
2021-02-18 21:19:39 +0530

    added class name

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
-    belongs_to :creator,class_name :'User'
+    belongs_to :creator, class_name: 'User',foreign_key: 'attendee_id'
     has_many :attendees, through: :invitation, source: :User
     scope :past, ->{where('date < ?', Date.today)}
     scope :upcoming, ->{where('date>=?', Date.today)}
