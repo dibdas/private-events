a503715c94221c536d77aa9db356961db403b47b
udberg <johnuduak066@gmail.com>
2021-02-18 15:45:42 +0100

    Fix migation errors

--- db/migrate/20210218124113_add_user_to_events.rb
+++
@@ -1,5 +0,0 @@
-class AddUserToEvents < ActiveRecord::Migration[6.1]
-  def change
-    add_column :events, :creator_id, :interger
-  end
-end
--- db/migrate/20210218124808_add_events_to_invitaions.rb
+++
@@ -1,5 +0,0 @@
-class AddEventsToInvitaions < ActiveRecord::Migration[6.1]
-  def change
-    add_column :invitaions, :attendee_event_id, :integer
-  end
-end
