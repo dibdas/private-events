ab9a1172e4b7fe7090b30c03f778549010a2aaa1
udberg <johnuduak066@gmail.com>
2021-02-23 08:38:56 +0100

    Remove if logical statements from view and update readme

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    def show_events(event)
+        if event.attendees.size.positive?
+            render '/events/show_events', event: event
+        else
+            render '/events/no_events'
+        end
+    end
 end
+
